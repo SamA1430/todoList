59086cc3418a730823c0aa267e1a156dc737c12c
SamA1430 <samuel.arg14@gmail.com>
2023-02-21 18:24:45 -0500

    add HTML CSS JS

---
+++ script.js
@@ -0,0 +1,17 @@
+$("document").ready(function(){
+    var number = 0;
+    function addItem(name){
+        if (!/\S/.test($("#new").val())){
+            $("#todoItems").append('<li class="items"><input type="checkbox">'+name+'<button type="button" class="delete">Delete</button></li>');
+        }};
+
+    $("body").on("click",".delete",function(){
+        $(this).parent().remove();
+    });
+
+    $("#addTodo").click(function(){
+        var name = $("#new").val();
+        addItem(name);
+        $("#new").val("")
+    })
+});
